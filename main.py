f9e31619a6a6bc8c38f9ac7afcc5fa1384e35aba
Maxtro2002 <jspr200231@gmail.com>
2023-04-19 11:38:31 -0500

    implementacion csv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import csv
 
 from promedioMovil import promedioMovil
@@ -7,10 +8,43 @@ from suavizamientoExpo import suavizado
 from proyeccionTendencia import proyeccionTend
 from analisisMinimosCuadrados import minimosCuadrados
 
-with open('Mobile phone price.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
+array = pd.read_csv('Mobile phone price.csv', header=0, delimiter=',')
+
+print("\n------------------------DATASET------------------------\n")
+print("\nQue datos deseas analizar:\n"
+      "1. Storage\n"
+      "2. RAM\n"
+      "3. ScreenSize\n"
+      "4. Battery\n"
+      "5. Price\n")
+datosDS = int(input("Ingrese el numero de los datos que desea usar:\n"))
+
+invalidoDatos = True
+
+while invalidoDatos:
+    if datosDS == 1:
+        data = array['Storage'].values
+        invalidoDatos = False
+
+    elif datosDS == 2:
+        data = array['RAM'].values
+        invalidoDatos = False
+
+    elif datosDS == 3:
+        data = array['ScreenSize'].values
+        invalidoDatos = False
+
+    elif datosDS == 4:
+        data = array['Battery'].values
+        invalidoDatos = False
+
+    elif datosDS == 5:
+        data = array['Price'].values
+        invalidoDatos = False
+
+    else:
+        print("ERROR: Por favor seleccione una opcion validas")
+        datosDS = int(input("Ingrese el numero de los datos que desea usar:\n"))
 
 print("1. Promedio movil\n"
       "2. suavizamiento exponencial\n"
@@ -20,52 +54,55 @@ print("1. Promedio movil\n"
 
 metodo = int(input("Ingrese el numero del metodo que desea usar:\n"))
 
-invalido = True
+invalidoMetodo = True
 
-data = np.array([10, 15, 13, 17, 20, 19, 22, 24, 23, 25])
-
-tam_ventana = 3
-alpha = 0.3
-periodos = 3
-
-
-while invalido:
+while invalidoMetodo:
     # ------------------------------------Promedio movil------------------------------------
 
     if metodo == 1:
+        tam_ventana = int(input("ingrese el tamaño de ventana que desea utilizar:\n"))
         prediccion = promedioMovil(data, tam_ventana)
 
         plt.plot(data, label='Datos')
         plt.plot([None] * (tam_ventana - 1) + prediccion, label='Prediccion')
         plt.legend()
         plt.show()
-        invalido = False
+        invalidoMetodo = False
 
     elif metodo == 2:
         # ------------------------------------Suavizado exponencial------------------------------------
+        alpha = float(input("ingrese el valor de alpha que desea utilizar (valores de 0 a 1):\n"))
         prediccion = suavizado(data, alpha)
 
         plt.plot(data, label='Datos')
         plt.plot(prediccion, label='Prediccion')
         plt.legend()
         plt.show()
-        invalido = False
+        invalidoMetodo = False
 
     elif metodo == 3:
         # ------------------------------------Proyeccion de tendencia------------------------------------
+        periodos = int(input("ingrese la cantidad de periodos que desea utilizar:\n"))
         projection = proyeccionTend(data, periodos)
 
         plt.plot(data, label='Datos')
         plt.plot([None] * len(data) + projection, label='Proyeccion')
         plt.legend()
         plt.show()
+        invalidoMetodo = False
 
     elif metodo == 4:
         # ------------------------------------Regrecion minimos cuadrados------------------------------------
+        periodos = int(input("ingrese la cantidad de periodos que desea utilizar:\n"))
         projection = minimosCuadrados(data, periodos)
 
         plt.plot(data, label='Datos')
         plt.plot([None] * len(data) + projection, label='Proyeccion')
         plt.legend()
         plt.show()
+        invalidoMetodo = False
+
+    else:
+        print("ERROR: Por favor seleccione una opcion validas")
+        metodo = int(input("Ingrese el numero del metodo que desea usar:\n"))
 
