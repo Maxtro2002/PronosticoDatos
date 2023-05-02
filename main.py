da9e16492147332e42c9f201c42414f3d60d0d16
Maxtro2002 <jspr200231@gmail.com>
2023-05-02 12:12:03 -0500

    Cambio dataset

--- main.py
+++ main.py
@@ -6,97 +6,71 @@ from suavizamientoExpo import suavizado
 from suavizadoConTendencia import suavizadoConTendencia
 from minimosCuadrados import minimosCuadrados
 
-array = pd.read_csv('Mobile phone price.csv', header=0, delimiter=',')
-print("\n------------------------DATASET------------------------\n")
-print("\nQue datos deseas analizar:\n"
-      "1. Storage\n"
-      "2. RAM\n"
-      "3. ScreenSize\n"
-      "4. Battery\n"
-      "5. Price\n")
-datosDS = int(input("Ingrese el numero de los datos que desea usar:\n"))
+"""
+Metodos utilizados para analisis:
+Promedio movil
+Suavizado exponencial
+Suavizado exponencial con tendencia
+Minimos cuadrados
+"""
 
-invalidoDatos = True
 
-while invalidoDatos:
-    if datosDS == 1:
-        data = array['Storage'].values
-        invalidoDatos = False
+array = pd.read_csv('ds_salaries.csv', header=0, delimiter=',')
+print(array["salary_in_usd"])
+wait = input("")
+repetir = True
+while repetir:
+    print("\n------------------------DATASET------------------------\n")
+    data = array["salary_in_usd"].values
 
-    elif datosDS == 2:
-        data = array['RAM'].values
-        invalidoDatos = False
-
-    elif datosDS == 3:
-        data = array['ScreenSize'].values
-        invalidoDatos = False
-
-    elif datosDS == 4:
-        data = array['Battery'].values
-        invalidoDatos = False
-
-    elif datosDS == 5:
-        data = array['Price'].values
-        invalidoDatos = False
-
-    else:
-        print("ERROR: Por favor seleccione una opcion validas")
-        datosDS = int(input("Ingrese el numero de los datos que desea usar:\n"))
-
-print("1. Promedio movil\n"
-      "2. suavizamiento exponencial\n"
-      "3. Suavizado con tendencia\n"
-      "4. Minimos cuadrados\n"
-      )
-
-metodo = int(input("Ingrese el numero del metodo que desea usar:\n"))
-
-invalidoMetodo = True
-
-while invalidoMetodo:
     # ------------------------------------Promedio movil------------------------------------
+    tam_ventana = 3
+    prediccionM = promedioMovil(data, tam_ventana)
+    print("Promedio movil")
+    plt.plot(data, label='Datos')
+    plt.plot([None] * (tam_ventana - 1) + prediccionM, label='prediccionM')
+    plt.legend()
+    plt.show()
+    plt.plot(prediccionM, 'o', color="blue", label='Tendencia')
+    plt.show()
 
-    if metodo == 1:
-        tam_ventana = int(input("ingrese el tamaño de ventana que desea utilizar:\n"))
-        prediccion = promedioMovil(data, tam_ventana)
+    # ------------------------------------Suavizado exponencial------------------------------------
+    prediccion = suavizado(data)
+    print("Suavizado exponencial")
+    plt.plot(data, label='Datos')
+    plt.plot(prediccion, label='Prediccion')
+    plt.legend()
+    plt.show()
+    plt.plot(prediccion, 'o', color="blue", label='Tendencia')
+    plt.show()
 
-        plt.plot(data, label='Datos')
-        plt.plot([None] * (tam_ventana - 1) + prediccion, label='Prediccion')
-        plt.legend()
-        plt.show()
-        invalidoMetodo = False
 
-    elif metodo == 2:
-        # ------------------------------------Suavizado exponencial------------------------------------
-        prediccion = suavizado(data)
 
-        plt.plot(data, label='Datos')
-        plt.plot(prediccion, label='Prediccion')
-        plt.legend()
-        plt.show()
-        invalidoMetodo = False
+    # ------------------------------------Suavizado con tendencia------------------------------------
+    proyeccion = suavizadoConTendencia(data)
+    print("Suavizado con tendencia")
+    plt.plot(data, label='Datos')
+    plt.plot([None] * len(data) + proyeccion, label='Proyeccion')
+    plt.legend()
+    plt.show()
+    plt.plot(proyeccion, 'o', color="blue", label='Tendencia')
+    plt.show()
 
-    elif metodo == 3:
-        # ------------------------------------Suavizado con tendencia------------------------------------
-        proyeccion = suavizadoConTendencia(data)
 
-        plt.plot(data, label='Datos')
-        plt.plot([None] * len(data) + proyeccion, label='Proyeccion')
-        plt.legend()
-        plt.show()
-        invalidoMetodo = False
+    # ------------------------------------Minimos cuadrados------------------------------------
+    proyeccion = minimosCuadrados(data)
+    print("Minimos cuadrados")
+    plt.plot(data, label='Datos')
+    plt.plot([None] * len(data) + proyeccion, label='Proyeccion')
+    plt.legend()
+    plt.show()
+    plt.plot(proyeccion, 'o', color="blue", label='Tendencia')
+    plt.show()
 
-    elif metodo == 4:
-        # ------------------------------------Minimos cuadrados------------------------------------
-        proyeccion = minimosCuadrados(data)
 
-        plt.plot(data, label='Datos')
-        plt.plot([None] * len(data) + proyeccion, label='Proyeccion')
-        plt.legend()
-        plt.show()
-        invalidoMetodo = False
 
-    else:
-        print("ERROR: Por favor seleccione una opcion validas")
-        metodo = int(input("Ingrese el numero del metodo que desea usar:\n"))
-
+    reset = input("Desea analisar otro dato?\n"
+                      "1. Si\n"
+                      "2. No\n")
+    if reset != "1":
+        repetir = False
